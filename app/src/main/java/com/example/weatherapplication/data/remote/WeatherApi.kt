197dca0c079ee35be97881dca15300e2a9e2caeb
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-07-05 13:24:27 +0100

    Updated Code Commit

--- app/src/main/java/com/example/weatherapplication/data/remote/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapplication/data/remote/WeatherApi.kt
@@ -1,4 +1,13 @@
 package com.example.weatherapplication.data.remote
 
-class WeatherApi {
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    suspend fun getWeatherData(
+        @Query("latitude") lat: Double,
+        @Query("longitude") long: Double
+    ) : WeatherDto
 }
