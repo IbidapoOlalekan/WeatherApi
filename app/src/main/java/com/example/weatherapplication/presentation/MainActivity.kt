69d5bd616d4b54acc5578bbaa7734ff78ec4936b
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-07-06 22:16:05 +0100

    Updated Code Commit

--- app/src/main/java/com/example/weatherapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/presentation/MainActivity.kt
@@ -3,26 +3,47 @@ package com.example.weatherapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherapplication.presentation.ui.theme.WeatherApplicationTheme
+import java.util.jar.Manifest
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
         setContent {
             WeatherApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.DarkGray)
                 ) {
-                    Greeting("Android")
+                    WeatherCard(
+                        state = viewModel.state,
+                        backgroundColor = Color.Blue
+                    )
                 }
             }
         }
